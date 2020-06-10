0b5940ce62a11a36cbb0536d5a453ea269c27356
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-06-10 16:45:35 -0400

    working on last question

--- index.js
+++ index.js
@@ -143,12 +143,16 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(arr, newArr){
+function copy(arr){
 
-    newArr = [...arr];
-    return newArr;
+    newFlavors = [...arr];
+    return newFlavors;
 
 }
+
+
+
+console.log('6', copy(originalFlavors));
 
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
@@ -166,9 +170,13 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
+function filterByWord(arr, flavor){
+    let 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].includes(flavor)) {
+            
+        }
+    }
 
 }
 
@@ -274,8 +282,23 @@ var regionalFlavors = ["Pink Bubblegum",
     "Chocolate Chocolate Chip Cheesecake",
     "Caramel 'n' Cookies"]
 
-function getRandomFlavors(/*code here*/){
+function getRandomFlavors(arr1){
+    let bestFlavors = []
+    for (let i = 0; i < 10; i++) {
+        let index = Math.floor(Math.random() * 31)
+        bestFlavors.push(arr1[index])
+    }
+    return bestFlavors;
 
-    /*code here*/
+    // for (let i = 0; i < arr2.length; i++) {
+        
+    // }
+
+    // for (let i = 0; i < arr3.length; i++) {
+        
+    // }
 
 }
+
+
+console.log(getRandomFlavors(newFlavors));
